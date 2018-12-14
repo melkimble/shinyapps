87380c239397126823bbd3eadcb710a0ca865f0c
Mel <mkimble88@gmail.com>
2018-12-14 02:30:06 -0500

    Update server.R

--- DMRData_LPA_SST/server.R
+++ DMRData_LPA_SST/server.R
@@ -199,17 +199,17 @@ function(input, output) {
     MonthOrder<-c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec")
     TheTable<-TheTable[order(factor(TheTable$Month,levels=c(MonthOrder))),]
     
-    names(TheTable)<-c("Month", "SST", "SD", "Bathy")
+    names(TheTable)<-c(" Month ", " SST ", " SD ", " Bathy ")
     
     content <- paste("<h4> Site ID:", ID,"</h4>",
                      "<strong>", sprintf("%s, %s", selectedSite$species[1], 
-                                         selectedSite$equipment[1]),"</strong> </br>", 
+                                         selectedSite$equipment[1]),"</strong>", 
                      htmlTable(TheTable, col.rgroup = c("none", "#F9FAF0"), 
                                col.columns = c("none", "#F1F0FA"),
                                align.header = "clcr",
                                align.cgroup = "lcr",
                                padding.rgroup = "&nbsp;&nbsp;"),
-                     "*Sea Surface Temperature (SST, C)", "Bathymetry (m).")
+                     "*Sea Surface Temperature (SST, C)","</br>", "Bathymetry (m).")
     leafletProxy("map") %>% addPopups(lng, lat, content, layerId = ID)
   }
 
