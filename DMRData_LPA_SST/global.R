c31a69de53a9ea63241832273b849a88e0308bba
Mel <mkimble88@gmail.com>
2018-12-13 23:45:35 -0500

    bug fix

--- DMRData_LPA_SST/global.R
+++ DMRData_LPA_SST/global.R
@@ -3,15 +3,20 @@ library(dplyr)
 DMRData <- readRDS("data/TestData.rds")
 DMRDataMelt <- readRDS("data/TestDataMelt.rds")
 DMRDataMeltAgg <- readRDS("data/TestDataMeltAgg.rds")
+DMRDataMeltMonthAgg<- readRDS("data/TestDataMeltMonthAgg.rds")
 
-names(DMRData)<-c("ID", "SITE_ID", "latitude", "longitude", "species", "equipment", "LEASE_TYPE", "Status", "TheYr", "EndYr", "BATHY", 
+names(DMRData)<-c("ID", "SITE_ID", "latitude", "longitude", "species", "equipment", "LEASE_TYPE", "Status", "StartYr", "EndYr", "BATHY", 
                   "X010029_20130824", "X010029_20140115", "X010029_20140304", "X010029_20140507", "X010029_20140608", "X010029_20140827",
                   "X010029_20140912", "X010029_20140928", "X010029_20141030", "X010029_20150307", "X010029_20150713", "X010029_20150729",
                   "X010029_20150814", "X010029_20150915", "X010029_20151220", "X011029_20160823", "X011030_20130714", "X011030_20130730",
                   "X011030_20130815", "X011030_20131002", "X011030_20131018", "X011030_20140327", "X011030_20140412", "X011030_20140530",
                   "X011030_20140919", "X011030_20150415", "X011030_20150906", "X011030_20151008", "X011030_20151109", "X011030_20151125",
                   "X011030_20160128", "X011030_20160417", "X011030_20160620", "X011030_20160706", "X011030_20160722", "X011030_20160823")
-names(DMRDataMelt)<-c("SITE_ID", "LSAT8_PPPRRR", "Date", "SST", "latitude", "longitude", "species")
+names(DMRDataMelt)<-c("SITE_ID", "LSAT8_PPPRRR", "Date", "SST", "latitude", "longitude", "species", "BATHY", "Datef", "Month")
+## this data does not have a date because it is aggregated on SITE_ID. So all SST values are averaged by SITE_ID.
+names(DMRDataMeltAgg)<-c("ID","SITE_ID", "latitude", "longitude", "species", "equipment", "SST_Mean", "SST_StdDev", "BATHY")
+## this data has month because SST is aggregated by SITE_ID and Month.
+names(DMRDataMeltMonthAgg)<-c("ID","SITE_ID", "latitude", "longitude", "Month", "species", "equipment", "SST_Mean", "SST_StdDev", "BATHY")
 
 #DMRData$ID<-as.integer(DMRData$ID)
 #DMRData$SITE_ID <- as.character(DMRData$SITE_ID)
@@ -20,21 +25,19 @@ names(DMRDataMelt)<-c("SITE_ID", "LSAT8_PPPRRR", "Date", "SST", "latitude", "lon
 #DMRData$species <- as.character(DMRData$species)
 #DMRData$equipment <- as.character(DMRData$equipment)
 #DMRData$SST<-round(DMRData$SST,2) 
-DMRData$BATHY<-round(DMRData$BATHY,2)
 #DMRData$SeedDist<-round(DMRData$SeedDist,2)
+DMRData$BATHY<-round(DMRData$BATHY,2)
 
 DMRDataMelt$SST <- round(DMRDataMelt$SST,2)
-DMRDataMelt$Datef<-format(as.Date(as.character(DMRDataMelt$Date), "%Y%m%d"),"%Y-%m-%d")
-DMRDataMelt$Month<-format(as.Date(as.character(DMRDataMelt$Date), "%Y%m%d"),"%b")
+DMRDataMelt$BATHY <- round(DMRDataMelt$BATHY,2)
 
-#length(unique(DMRDataMeltAgg$SITE_ID))
-DMRDataMeltAgg2 <- group_by(DMRDataMeltAgg, SITE_ID) %>% summarise(SST_Mean = mean(SST_Mean), 
-                                                                   BATHY = mean(BATHY))
-#length(unique(DMRData$SITE_ID))
-ColKeep<-c("SITE_ID","LATITUDE","LONGITUDE", "species", "equipment")
-DMRData_LPA_Merge_ColSub<-DMRData[,ColKeep]
-test<-DMRDataMeltAgg2 %>% left_join(DMRData_LPA_Merge_ColSub, by="SITE_ID")
-names(DMRDataMeltAgg2)<-c("SITE_ID", "LSAT8_PPPRRR", "Date", "SST", "latitude", "longitude", "species")
+DMRDataMeltAgg$SST_Mean <- round(DMRDataMeltAgg$SST_Mean,2)
+DMRDataMeltAgg$SST_StdDev <- round(DMRDataMeltAgg$SST_StdDev,2)
+DMRDataMeltAgg$BATHY <- round(DMRDataMeltAgg$BATHY,2)
+
+DMRDataMeltMonthAgg$SST_Mean <- round(DMRDataMeltMonthAgg$SST_Mean,2)
+DMRDataMeltMonthAgg$SST_StdDev <- round(DMRDataMeltMonthAgg$SST_StdDev,2)
+DMRDataMeltMonthAgg$BATHY <- round(DMRDataMeltMonthAgg$BATHY,2)
 
 #row.names(DMRData) <- DMRData$ID
 
