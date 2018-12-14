59028a1d4a084d4d68479d821694e459d0f417af
Mel <mkimble88@gmail.com>
2018-12-14 02:34:27 -0500

    Update server.R

--- DMRData_LPA_SST/server.R
+++ DMRData_LPA_SST/server.R
@@ -208,7 +208,7 @@ function(input, output) {
                                col.columns = c("none", "#F1F0FA"),
                                align.header = "clcr",
                                align.cgroup = "lcr",
-                               padding.rgroup = "&nbsp;&nbsp;"),
+                               padding.tspanner = "&nbsp;&nbsp;"),
                      "*Sea Surface Temperature (SST, C)","</br>", "Bathymetry (m).")
     leafletProxy("map") %>% addPopups(lng, lat, content, layerId = ID)
   }
