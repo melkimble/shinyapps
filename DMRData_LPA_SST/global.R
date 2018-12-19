c0703bf96906cecdcdfa3a557648634c4869029e
Mel <mkimble88@gmail.com>
2018-12-18 21:56:11 -0500

    Update global.R

--- DMRData_LPA_SST/global.R
+++ DMRData_LPA_SST/global.R
@@ -5,14 +5,14 @@ DMRDataMelt <- readRDS("data/TestDataMelt.rds")
 DMRDataMeltAgg <- readRDS("data/TestDataMeltAgg.rds")
 DMRDataMeltMonthAgg<- readRDS("data/TestDataMeltMonthAgg.rds")
 
-names(DMRData)<-c("ID", "SITE_ID", "latitude", "longitude", "speciesCategory", "equipment", "LEASE_TYPE", "Status", "StartYr", "EndYr", "BATHY", 
+names(DMRData)<-c("ID", "SITE_ID", "latitude", "longitude", "species", "speciesCategory", "equipment", "LEASE_TYPE", "Status", "StartYr", "EndYr", "BATHY", 
                   "X010029_20130824", "X010029_20140115", "X010029_20140304", "X010029_20140507", "X010029_20140608", "X010029_20140827",
                   "X010029_20140912", "X010029_20140928", "X010029_20141030", "X010029_20150307", "X010029_20150713", "X010029_20150729",
                   "X010029_20150814", "X010029_20150915", "X010029_20151220", "X011029_20160823", "X011030_20130714", "X011030_20130730",
                   "X011030_20130815", "X011030_20131002", "X011030_20131018", "X011030_20140327", "X011030_20140412", "X011030_20140530",
                   "X011030_20140919", "X011030_20150415", "X011030_20150906", "X011030_20151008", "X011030_20151109", "X011030_20151125",
                   "X011030_20160128", "X011030_20160417", "X011030_20160620", "X011030_20160706", "X011030_20160722", "X011030_20160823")
-names(DMRDataMelt)<-c("SITE_ID", "LSAT8_PPPRRR", "Date", "SST", "latitude", "longitude", "speciesCategory", "BATHY", "Datef", "Month")
+names(DMRDataMelt)<-c("SITE_ID", "LSAT8_PPPRRR", "Date", "SST", "latitude", "longitude", "species", "speciesCategory", "BATHY", "Datef", "Month")
 ## this data does not have a date because it is aggregated on SITE_ID. So all SST values are averaged by SITE_ID.
 names(DMRDataMeltAgg)<-c("ID","SITE_ID", "latitude", "longitude", "speciesCategory", "equipment", "SST", "SST_StdDev", "BATHY")
 ## this data has month because SST is aggregated by SITE_ID and Month.
@@ -83,6 +83,7 @@ cleantable <- DMRData %>%
     SiteId = SITE_ID,
     Lat = latitude,
     Long = longitude,
+    Species = species,
     SpeciesCategory = speciesCategory,
     Equipment = equipment,
     LeaseType = LEASE_TYPE,
