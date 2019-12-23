85edf501fabb5afbce4a3e6c455f314a3573b9a1
Melissa Kimble <mkimble88@gmail.com>
2019-12-23 05:29:01 -0500

    Created Cosine Similarity Rshiny Leaflet application. Modified from https://rstudio.github.io/leaflet/shiny.html

--- CosSim/server.R
+++ CosSim/server.R
@@ -1,348 +1,107 @@
+library(shiny)
 library(leaflet)
 library(RColorBrewer)
-library(scales)
-library(lattice)
-library(dplyr)
-library(ggplot2)
-library(htmlTable)
+library(rgdal)
+library(spdplyr)
+
+# https://rstudio.github.io/leaflet/shiny.html
+
 
 function(input, output, session) {
-
-  ## Interactive Map ###########################################
-
-  # Create the map
+  
+  # Reactive expression for the data subsetted to what the user selected
+  filteredData <- reactive({
+    ## input from the slider
+    CosSim_rds[CosSim_rds$cor >= input$range[1] & CosSim_rds$cor <= input$range[2],]
+  })
+  
+  # This reactive expression represents the palette function,
+  # which changes as the user makes selections in UI.
+  colorpal <- reactive({
+    colorNumeric(input$colors, NCT$meanTenure)
+  })
+  
+  
+  bins <- as.vector(quantile(NCT$meanTenure, na.rm=TRUE))
+  pal <- colorBin("YlOrRd", domain = NCT$meanTenure, bins = bins, na.color = "#808080")
+  
+  labels <- sprintf(
+    "<strong>%s</strong><br/>%g NCT",
+    NCT$Location_I, NCT$meanTenure) %>% 
+    lapply(htmltools::HTML)
+  
   output$map <- renderLeaflet({
-    leaflet() %>%
+    # Use leaflet() here, and only include aspects of the map that
+    # won't need to change dynamically (at least, not unless the
+    # entire map is being torn down and recreated).
+    leaflet() %>% 
       addTiles(
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
-        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
-      ) %>%
-      setView(lng = -67.709946, lat = 44.146299, zoom = 8)
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>') %>%
+      addPolygons(
+        data=NCT,
+        layerId=~Location_I,
+        fillColor = ~pal(meanTenure),
+        weight = 1,
+        opacity = 1,
+        color = "#444444",
+        dashArray = "3",
+        fillOpacity = 0.8,
+        highlight = highlightOptions(
+          color = "white", 
+          weight = 2,
+          bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto")) %>%
+      setView(lng = -69.7314453, lat = 43.9143787, zoom = 11)
+    
+
   })
   
-  # A reactive expression that returns the set of zips that are
-  # in bounds right now
-  meltMonthLeasesInBounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(DMRDataMeltMonthAgg[FALSE,])
-    bounds <- input$map_bounds
-    latRng <- range(bounds$north, bounds$south)
-    lngRng <- range(bounds$east, bounds$west)
-    ## user inputs based on what is selected from the timeSlider
-    yearRange<-input$timeSlider
+  # Reactive expression to create data frame of all input values ----
+  sliderValues <- reactive({
     
-    subset(DMRDataMeltMonthAgg,
-           latitude >= latRng[1] & latitude <= latRng[2] &
-             longitude >= lngRng[1] & longitude <= lngRng[2]& 
-             StartYr>=yearRange[1] & StartYr<=yearRange[2])
-  })
-  
-  # A reactive expression that returns the set of zips that are
-  # in bounds right now
-  meltLeasesInBounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(DMRDataMelt[FALSE,])
-    bounds <- input$map_bounds
-    latRng <- range(bounds$north, bounds$south)
-    lngRng <- range(bounds$east, bounds$west)
-    ## user inputs based on what is selected from the timeSlider
-    yearRange<-input$timeSlider
-    
-    subset(DMRDataMelt,
-           latitude >= latRng[1] & latitude <= latRng[2] &
-             longitude >= lngRng[1] & longitude <= lngRng[2] & 
-             StartYr>=yearRange[1] & StartYr<=yearRange[2])
-  })
-  observe({
-    selectPlot<-input$selectedplot
-    output$plot <- renderPlot({
-      if (selectPlot == "histTemp") {
-        # If no zipcodes are in view, don't plot
-        if (nrow(meltLeasesInBounds()) == 0)
-          return(NULL)
-        TheTitle=paste("Sea Surface Temperature (Mean:",round(mean(meltLeasesInBounds()$SST),digits=2),") at Aquaculture Sites",sep="")
-        ggplot(meltLeasesInBounds(), aes(x=SST)) +
-          theme(plot.title=element_text(hjust=0.5),
-                panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(),
-                panel.background = element_blank()) +
-          xlim(range(DMRDataMelt$SST)) +
-          geom_histogram(binwidth=1, colour="white", fill="#00DD00") +
-          geom_vline(aes(xintercept=mean(meltLeasesInBounds()$SST)),
-                     color="blue", linetype="dashed", size=1) +
-          ggtitle(TheTitle) +
-          xlab("Temperature (C)") +
-          ylab("Frequency")
-        } else if (selectPlot == "histBathy") {
-          TheTitle=paste("Bathymetry (Mean:",round(mean(na.omit(meltLeasesInBounds()$BATHY)),digits=2),") at Aquaculture Sites",sep="")
-          ggplot(na.omit(meltLeasesInBounds()), aes(x=BATHY)) +
-            theme(plot.title=element_text(hjust=0.5),
-                  panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(),
-                  panel.background = element_blank()) +
-            xlim(range(DMRDataMelt$BATHY)) +
-            geom_histogram(binwidth=1, colour="white", fill="#00DD00") +
-#            geom_vline(aes(xintercept=mean(meltLeasesInBounds()$BATHY)),
-#                       color="blue", linetype="dashed", size=1) +
-            ggtitle(TheTitle) +
-            xlab("Bathymetry (m)") +
-            ylab("Frequency")
-          
-        } else if (selectPlot == "scatterspeciesTemp") {
-          # If no zipcodes are in view, don't plot
-          if (nrow(meltMonthLeasesInBounds()) == 0)
-            return(NULL)
-          TheTitle=paste("Sea Surface Temperature at Aquaculture Sites",sep="")
-          ggplot(meltMonthLeasesInBounds(), aes(x=Month, y=SST, color=speciesCategory, shape=speciesCategory)) +
-            theme(plot.title=element_text(hjust=0.5),
-                  panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(),
-                  panel.background = element_blank(),
-                  axis.text=element_text(size=12),
- #                 axis.text.x = element_text(angle=35, hjust=1),
-                  axis.title.x = element_blank(),
-                  legend.position="top",
-                  legend.text=element_text(size=12),
-                  legend.title=element_blank()) +
-            scale_x_discrete(limits = month.abb) +
-            geom_point(size = 3) +
-#            scale_x_date(date_labels = "%b %Y", date_breaks="3 month") +
-#            ggtitle(TheTitle) +
-            ylab("Temperature (C)")
-          } else if (selectPlot == "boxSpeciesTemp") {
-            # If no zipcodes are in view, don't plot
-            if (nrow(meltMonthLeasesInBounds()) == 0)
-              return(NULL)
-            #MonthOrder<-c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec")
-            ggplot(meltMonthLeasesInBounds(), aes(x=Month, y=SST, fill=speciesCategory)) +
-              geom_boxplot() +
-              theme(plot.title=element_text(hjust=0.5),
-                    panel.grid.major = element_blank(),
-                    panel.grid.minor = element_blank(),
-                    panel.background = element_blank(),
-                    axis.text=element_text(size=12),
-                    legend.text=element_text(size=12),
-                    legend.title=element_blank(),
-                    legend.position="top",
-                    axis.title.x=element_blank()) +
-              scale_x_discrete(limits = month.abb) +
-#              scale_x_date(date_labels = "%b", date_breaks="1 month") +
-#              ggtitle("Monthly Temperature by SpeciesCategory") +
-              ylab("Temperature (C)")
-            } else if (selectPlot == "boxSpeciesBathy") {
-              # If no zipcodes are in view, don't plot
-              if (nrow(meltMonthLeasesInBounds()) == 0)
-                return(NULL)
-              ggplot(meltMonthLeasesInBounds()[!is.na(meltMonthLeasesInBounds()$BATHY),], aes(x=speciesCategory, y=BATHY, fill=speciesCategory)) +
-                geom_boxplot() +
-                theme(legend.position="none",
-                      plot.title=element_text(hjust=0.5),
-                      panel.grid.major = element_blank(),
-                      panel.grid.minor = element_blank(),
-                      panel.background = element_blank(),
-                      axis.text=element_text(size=12),
-                      axis.title.x=element_blank()) +
-#                scale_x_date(date_labels = "%b", date_breaks="1 month") +
-#                ggtitle("Bathymetry by SpeciesCategory") +
-                ylab("Bathymetry (m)")
-              } else return(NULL)
-      }) 
-    })
-  # This observer is responsible for maintaining the circles and legend,
-  # according to the variables the user has chosen to map to color and size.
-  observe({
-    ## user inputs based on what is selected from the timeSlider
-    yearRange<-input$timeSlider
-    TheStart<-yearRange[1]
-    TheEnd<-yearRange[2]
-    DMRDataMeltAggTimeSub<-DMRDataMeltAgg[DMRDataMeltAgg$StartYr>=TheStart & DMRDataMeltAgg$StartYr<=TheEnd,]
-    
-    ## user inputs based on what is selected from the drop down menu
-    colorBy <- input$color
-#    sizeBy <- input$size
-    if (colorBy == "SST") {
-      # Color and palette are treated specially in the "SST" case, because
-      # the values are categorical instead of continuous.
-      ## this input$threshold is from the ui.R script, it grabs the value input by the user
-      ## and adjusts the threshold on the size of the icons based on the threshold.
-      # Precalculate the breaks we'll need for the two histograms
-      #colorBy="SST"
-      
-      colorData <- DMRDataMeltAggTimeSub[[colorBy]]
-      pal <- colorBin("RdYlBu", colorData, 7, pretty = FALSE, reverse = TRUE)
-    } else if (colorBy == "BATHY") {
-      colorData <- DMRDataMeltAggTimeSub[[colorBy]]
-      colorData <- DMRDataMeltAggTimeSub[["BATHY"]]
-      pal <- colorBin("Greys", colorData, 7, pretty = FALSE, reverse = TRUE)
-    } else {
-      colorData <-as.factor(DMRDataMeltAggTimeSub[[colorBy]])
-      pal <- colorFactor("viridis", colorData)
-    }
-    #sizeBy="speciesCategory"
-#    if (sizeBy == "speciesCategory") {
-      # Radius is treated specially in the "speciesCategory" case.
-#      radius <-  hist(plot = FALSE,as.numeric(as.factor(DMRDataMeltAggTimeSub[[sizeBy]])), breaks=7)$breaks*100
-#    } else if (sizeBy == "equipment") {
-#      radius <- hist(plot = FALSE,as.numeric(as.factor(DMRDataMeltAggTimeSub[[sizeBy]])), breaks=7)$breaks*10
-#    } else {
-#      radius <- hist(plot = FALSE,as.numeric(as.factor(DMRDataMeltAggTimeSub[[sizeBy]])), breaks=7)$breaks
-     # radius <- DMRDataMeltAggTimeSub[[sizeBy]] / max(DMRDataMeltAggTimeSub[[sizeBy]]) * 10000
-#    }
-    
-    leafletProxy("map", data = DMRDataMeltAggTimeSub) %>%
-      clearShapes() %>%
-      addCircles(~longitude, ~latitude, radius= 150, layerId=~unique(SITE_ID),
-                 stroke=FALSE, fillOpacity=0.8, fillColor=pal(colorData)) %>%
-      addLegend("bottomleft", pal=pal, values=colorData, title=colorBy,
-                layerId="colorLegend")
-  })
-
-  # Show a popup at the given location
-  ## later grab SITE_ID from this group and calculate aggregates based on
-  
-  # DMRData DMRDataMelt DMRDataMeltAgg DMRDataMeltMonthAgg
-  
-  showSitePopup <- function(ID, lat, lng) {
-    #print(ID)
-    #ID="JYOU215"
-    #head(DMRDataMelt)
-    
-    # popupPlot<-renderPlot({ggplot(selectedSite, aes(x=Month, y=SST, color=SST)) +
-    #              theme(panel.grid.major = element_blank(),
-    #                    panel.grid.minor = element_blank(),
-    #                    panel.background = element_blank(),
-    #                    axis.text=element_text(size=12),
-    #                    #                 axis.text.x = element_text(angle=35, hjust=1),
-    #                    axis.title.x = element_blank(),
-    #                    legend.position="none") +
-    #              scale_x_discrete(limits = month.abb) +
-    #              geom_point(size = 3) +
-    #              geom_smooth(aes(group=1), se=FALSE, color="black") +
-    #              ylab("Temperature (C)")})
-
-    
-    selectedSite <- DMRDataMeltMonthAgg[DMRDataMeltMonthAgg$SITE_ID == ID,]
-    TheID <- DMRDataMeltMonthAgg$SITE_ID[DMRDataMeltMonthAgg$SITE_ID == ID][1]
-    lat <- DMRDataMeltMonthAgg$latitude[DMRDataMeltMonthAgg$SITE_ID == ID][1]
-    lng <- DMRDataMeltMonthAgg$longitude[DMRDataMeltMonthAgg$SITE_ID == ID][1]
-    siteStart <- DMRDataMeltMonthAgg$StartYr[DMRDataMeltMonthAgg$SITE_ID == ID][1]
-    siteEnd <- DMRDataMeltMonthAgg$EndYr[DMRDataMeltMonthAgg$SITE_ID == ID][1]
-
-    #print(paste(siteStart,siteEnd))
-    
-    Months<-selectedSite$Month
-    Temps<-selectedSite$SST
-    SDTemps<-selectedSite$SST_StdDev
-    
-    TheTable<-selectedSite[,c("Month","SST","SST_StdDev")]
-    MonthOrder<-c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec")
-    TheTable<-TheTable[order(factor(TheTable$Month,levels=c(MonthOrder))),]
-    
-    names(TheTable)<-c("Month", "Mean Temp", "Std Dev")
-    
-    content <- paste("<h4> Site ID: ", ID," (",siteStart,"-",siteEnd,")","</h4>",
-                     htmlTable(TheTable,
-                               col.rgroup = c("none", "#F9FAF0"),
-                               col.columns = c("none", "#F1F0FA"),
-                               align.header = "c",
-                               align.cgroup = "lcr",
-                               padding.tspanner = "&nbsp;&nbsp;",
-                               caption=paste("<strong>Species:</strong> ", selectedSite$species[1],
-                                             "</br>","<strong>Equipment:</strong> ", selectedSite$equipment[1],
-                                             "</br>","<strong>Bathymetry:</strong> ",selectedSite$BATHY[1]," meters",sep=""),
-                               tfoot="<i>Sea Surface Temperature (C)</i>"), sep="")
-    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = TheID)
-  }
-
-  # When map is clicked, show a popup with site info
-  observe({
-    leafletProxy("map") %>% clearPopups()
-    event <- input$map_shape_click
-    if (is.null(event))
+    click <- input$map_shape_click
+    if(is.null(click))
       return()
+    
+    idx <- na.omit(CosSim_rds[CosSim_rds$row == click$id,])
+    
+    idx <- idx[(idx$Similarity >= input$range[1]) & (idx$Similarity <= input$range[2]),]
+    
 
-    isolate({
-      showSitePopup(event$id, event$lat, event$lng)
-    })
+    
+    selected_polygon <- NCT %>% filter(NCT@data[["Location_I"]] %in% unique(idx$Station))
+    
+    leafletProxy("map") %>% clearGroup("highlighted_polygon")
+
+    #add a slightly thicker red polygon on top of the selected one
+    leafletProxy("map") %>% addPolylines(stroke=TRUE, weight = 3,color="red",data=selected_polygon,group="highlighted_polygon")
+
+
+    idx<-idx[c("Station","Similarity")]
+    idx <- idx[order(idx$Similarity,decreasing = TRUE),]
   })
-
-
-  ## Data Explorer ###########################################
- # DMRData
-  #sort(unique(cleantable$LeaseType))
-  #names(cleantable)
-  #?structure
+  
+  # Show the values in an HTML table ----
+  output$values <- renderTable({
+    sliderValues()
+    #remove any previously highlighted polygon
+  })
+  
+  # Use a separate observer to recreate the legend as needed.
   observe({
-    equipment <- if (is.null(input$speciesCategory)) character(0) else {
-      filter(cleantable, SpeciesCategory %in% input$speciesCategory) %>%
-        `$`('Equipment') %>%
-        unique() %>%
-        sort()
+    proxy <- leafletProxy("map", data=NCT)
+    
+    # Remove any existing legend, and only if the legend is
+    # enabled, create a new one.
+    proxy %>% clearControls()
+    if (input$legend) {
+      proxy %>% addLegend(pal = pal, values = ~meanTenure, opacity = 0.8, title = "Normalized Tenure",
+                          position = "bottomleft")
     }
-    stillSelected <- isolate(input$equipment[input$equipment %in% equipment])
-    updateSelectInput(session, "equipment", choices = equipment,
-      selected = stillSelected)
-  })
-  observe({
-    leasetype <- if (is.null(input$speciesCategory)) character(0) else {
-      cleantable %>%
-        filter(SpeciesCategory %in% input$speciesCategory,
-          is.null(input$equipment) | Equipment %in% input$equipment) %>%
-        `$`('LeaseType') %>%
-        unique() %>%
-        sort()
-    }
-    stillSelected <- isolate(input$leasetype[input$leasetype %in% leasetype])
-    updateSelectInput(session, "leasetype", choices = leasetype,
-      selected = stillSelected)
-  })
-
-  observe({
-    if (is.null(input$goto))
-      return()
-    isolate({
-      map <- leafletProxy("map")
-      map %>% clearPopups()
-      dist <- 0.05
-      
-      lat <- input$goto$lat
-      lng <- input$goto$lng
-      site <- as.character(cleantable$SiteId[cleantable$Lat == lat & cleantable$Long == lng])
-
-#      print(paste(site, lat, lng, sep=" "))
-      showSitePopup(site, lat, lng)
-      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist*10)
-    })
-  })
-
-  output$dmrTable <- DT::renderDataTable({
-    df <- cleantable %>%
-      filter(
-        Bathymetry >= input$minBathy,
-        Bathymetry <= input$maxBathy,  
-        is.null(input$speciesCategory) | SpeciesCategory %in% input$speciesCategory,
-        is.null(input$equipment) | Equipment %in% input$equipment,
-        is.null(input$leasetype) | LeaseType %in% input$leasetype, 
-        SAT010029_20130824 | SAT010029_20140115 | SAT010029_20140304 | SAT010029_20140507 | SAT010029_20140608
-        | SAT010029_20140827 | SAT010029_20140912 | SAT010029_20140928 | SAT010029_20141030 | SAT010029_20150307
-        | SAT010029_20150713 | SAT010029_20150729 | SAT010029_20150814 | SAT010029_20150915 | SAT010029_20151220
-        | SAT011029_20160823 | SAT011030_20130714 | SAT011030_20130730 | SAT011030_20130815 | SAT011030_20131002
-        | SAT011030_20131018 | SAT011030_20140327 | SAT011030_20140412 | SAT011030_20140530 | SAT011030_20140919 
-        | SAT011030_20150415 | SAT011030_20150906 | SAT011030_20151008 | SAT011030_20151109 | SAT011030_20151125
-        | SAT011030_20160128 | SAT011030_20160417 | SAT011030_20160620 | SAT011030_20160706 | SAT011030_20160722
-        | SAT011030_20160823 >= input$minTemp,
-        SAT010029_20130824 | SAT010029_20140115 | SAT010029_20140304 | SAT010029_20140507 | SAT010029_20140608
-        | SAT010029_20140827 | SAT010029_20140912 | SAT010029_20140928 | SAT010029_20141030 | SAT010029_20150307
-        | SAT010029_20150713 | SAT010029_20150729 | SAT010029_20150814 | SAT010029_20150915 | SAT010029_20151220
-        | SAT011029_20160823 | SAT011030_20130714 | SAT011030_20130730 | SAT011030_20130815 | SAT011030_20131002
-        | SAT011030_20131018 | SAT011030_20140327 | SAT011030_20140412 | SAT011030_20140530 | SAT011030_20140919 
-        | SAT011030_20150415 | SAT011030_20150906 | SAT011030_20151008 | SAT011030_20151109 | SAT011030_20151125
-        | SAT011030_20160128 | SAT011030_20160417 | SAT011030_20160620 | SAT011030_20160706 | SAT011030_20160722
-        | SAT011030_20160823 <= input$maxTemp
-      ) %>%
-      mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-site="', SiteId, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
-    action <- DT::dataTableAjax(session, df)
-
-    DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
   })
 }
