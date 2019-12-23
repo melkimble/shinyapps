7b21c43abcfa90b97f7938d4365dcee439a72f80
Melissa Kimble <mkimble88@gmail.com>
2019-12-23 13:20:14 -0500

    Added comments to global on what commands were necessary to run install.packages(rgdal).

--- CosSim/global.R
+++ CosSim/global.R
@@ -1,6 +1,9 @@
 library(spdplyr)
 library(rgdal)
 
+# To install gdal: sudo apt install gdal-bin proj-bin libgdal-dev libproj-dev
+# https://community.rstudio.com/t/can-not-install-rgdal/33971
+# R -e "install.packages('rgdal',type='source')"
 
 NCT <- readOGR("data/WQWL_VoronoiPolys_V2_NCT_NAD83.shp", layer = "WQWL_VoronoiPolys_V2_NCT_NAD83", GDAL1_integer64_policy = TRUE)
 CosSim_rds<- readRDS("data/DMR_Lobo_Voronoi_CosSim_flat.rds")
