a1adb07f210e9464428f17f6a0a83f5a83963968
mkimble88 <mkimble88@gmail.com>
2019-12-27 04:52:50 -0400

    Added title to CosSim

--- CosSim/ui.R
+++ CosSim/ui.R
@@ -4,11 +4,12 @@ library(leaflet)
 
 
 bootstrapPage(
+  titlePanel("Aquaculture Lease Tenure"),
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", height = "65%"),
-  absolutePanel(top = 10, right = 5,
+  absolutePanel(top = 05, right = 5,
                 # Input: Simple integer interval ----
-                sliderInput("range", "Range:",
+                sliderInput("range", "Similarity Range:",
                             min = 0, max = 1,
                             value = c(0.7,0.90), step = 0.1)
 
