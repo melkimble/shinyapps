c378cb56bb33c5c0dc9630d9b275fc452ac08536
Melissa Kimble <mkimble88@gmail.com>
2019-12-30 14:41:46 -0500

    Added windowTitle in titlePanel in CosSim application.

--- CosSim/ui.R
+++ CosSim/ui.R
@@ -4,7 +4,7 @@ library(leaflet)
 
 
 bootstrapPage(
-  titlePanel(h1("Aquaculture Lease Tenure", align = "center")),
+  titlePanel(h1("Aquaculture Lease Tenure", align = "center"), windowTitle="Aquaculture Lease Tenure"),
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", height = "65%"),
   absolutePanel(top = 05, right = 5,
