bbc5977224cfe27876f8b1489ad97627cc1dd9d6
Melissa Kimble <mkimble88@gmail.com>
2019-12-30 14:38:54 -0500

    Centered titlePanel in CosSim application.

--- CosSim/ui.R
+++ CosSim/ui.R
@@ -4,18 +4,19 @@ library(leaflet)
 
 
 bootstrapPage(
-  titlePanel("Aquaculture Lease Tenure"),
+  titlePanel(h1("Aquaculture Lease Tenure", align = "center")),
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", height = "65%"),
   absolutePanel(top = 05, right = 5,
                 # Input: Simple integer interval ----
-                sliderInput("range", "Similarity Range:",
-                            min = 0, max = 1,
-                            value = c(0.7,0.90), step = 0.1)
+                checkboxInput("legend", "Show legend", TRUE)
+                
 
   ),
   wellPanel(
-    checkboxInput("legend", "Show legend", TRUE),
+    sliderInput("range", "Similarity Range:",
+                min = 0, max = 1,
+                value = c(0.7,0.90), step = 0.1),
     DT::dataTableOutput('theOutputTable')
   )
   )
