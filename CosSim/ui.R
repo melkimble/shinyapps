7610a1b21d2c7134b499e98b07e4d029e5a3bf81
Melissa Kimble <mkimble88@gmail.com>
2019-12-30 14:57:43 -0500

    Moved slider and checkbox to wellPanel.

--- CosSim/ui.R
+++ CosSim/ui.R
@@ -4,16 +4,15 @@ library(leaflet)
 
 
 bootstrapPage(
-  titlePanel(h1("Aquaculture Lease Tenure", align = "center"), windowTitle="Aquaculture Lease Tenure"),
+  
+  titlePanel(h1(id="title-header","Aquaculture Lease Tenure", align = "center"), windowTitle="Aquaculture Lease Tenure"),
+  tags$style(type="text/css", "#title-header {color: black;
+             font-size: 30px;
+             font-style: bold;}"),
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", height = "65%"),
-  absolutePanel(top = 05, right = 5,
-                # Input: Simple integer interval ----
-                checkboxInput("legend", "Show legend", TRUE)
-                
-
-  ),
   wellPanel(
+    checkboxInput("legend", "Show legend", TRUE),
     sliderInput("range", "Similarity Range:",
                 min = 0, max = 1,
                 value = c(0.7,0.90), step = 0.1),
