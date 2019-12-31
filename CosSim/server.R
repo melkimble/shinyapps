2122f965ce6e528e0155eb5cbf1a788c5a09a4ec
Melissa Kimble <mkimble88@gmail.com>
2019-12-30 19:48:19 -0500

    Commented out code for troubleshooting

--- CosSim/server.R
+++ CosSim/server.R
@@ -73,11 +73,11 @@ function(input, output, session) {
     
     ## CosSim
     #(input$corrVars)
-    if(is.null(input$corrVars)){
-      output$printOutput<-renderPrint("A variable selection must be made.", quoted=FALSE)
-      return()
+#    if(is.null(input$corrVars)){
+#      output$printOutput<-renderPrint("A variable selection must be made.", quoted=FALSE)
+#      return()
       
-    } else{
+#    } else{
       
       Combo_Sub<-Combo_rds[grep(paste(c(input$corrVars,"Station"), collapse="|"),colnames(Combo_rds))]
       
@@ -90,11 +90,11 @@ function(input, output, session) {
       idx <- idx[(idx$Similarity >= input$range[1]) & (idx$Similarity <= input$range[2]),]
       NumRows<-nrow(idx)
       
-      if (NumRows == 0) {
-        ## if there are no selections, then no polygons will be selected
-        output$printOutput<-renderPrint("No areas within desired correlation ranges.", quoted=FALSE)
-        leafletProxy("map") %>% clearGroup("highlighted_polygon")
-      } else {
+ #     if (NumRows == 0) {
+#        ## if there are no selections, then no polygons will be selected
+#        output$printOutput<-renderPrint("No areas within desired correlation ranges.", quoted=FALSE)
+#        leafletProxy("map") %>% clearGroup("highlighted_polygon")
+#      } else {
         
         output$printOutput<-renderPrint(sprintf("%s areas within desired correlation ranges.",NumRows), quoted=FALSE)
         
@@ -107,12 +107,12 @@ function(input, output, session) {
         #add a slightly thicker red polygon on top of the selected one
         leafletProxy("map") %>% addPolylines(stroke=TRUE, weight = 3,color="red",data=selected_polygon,group="highlighted_polygon")
         
-      }
+ #     }
       idx<-idx[c("Station","Similarity")]
       DT::datatable({idx <- idx[order(idx$Similarity,decreasing = TRUE),] 
       idx})
 
-    }
+#    }
     
   })
   
